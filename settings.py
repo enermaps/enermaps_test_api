077fb6abdd2a2c4220989f29d766010c610941ab
cedricserugendo <cedric.serugendo@crem.ch>
2021-04-07 18:54:23 +0200

    apply linter

--- settings.py
+++ settings.py
@@ -1,8 +1,6 @@
-
-
 def init():
-    global  GEOFILE_ENDPOINT 
-    global CM_ENDPOINT 
+    global GEOFILE_ENDPOINT
+    global CM_ENDPOINT
 
-    GEOFILE_ENDPOINT = 'http://127.0.0.1:7000/api/geofile/'
-    CM_ENDPOINT = 'http://127.0.0.1:7000/api/cm/'
+    GEOFILE_ENDPOINT = "http://127.0.0.1:7000/api/geofile/"
+    CM_ENDPOINT = "http://127.0.0.1:7000/api/cm/"
